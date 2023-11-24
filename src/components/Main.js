f830ac77f4b7ac11c41b9556882846ed5f91f970
uliana <uliana.neustroeva25@gmail.com>
2023-11-24 21:17:50 +0200

    Save data to local storage

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,9 @@ import React from "react";
 import CreateTask from "./CreateTask";
 import TaskList from "./TaskList";
 
-const tasks = [];
+const tasks = localStorage.getItem("tasks")
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -17,20 +19,24 @@ export default class Main extends React.Component {
     }
     tasks.push({ task, isCompleted: false });
     this.setState({ tasks: tasks });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   };
   toggleTask = (taskId) => {
     const taskItem = tasks[taskId];
     taskItem.isCompleted = !taskItem.isCompleted;
     this.setState({ tasks: tasks });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   };
   deleteTask = (taskId) => {
     tasks.splice(taskId, 1);
     this.setState({ tasks: tasks });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   };
   editTask = (taskId, task) => {
     const taskItem = tasks[taskId];
     taskItem.task = task;
     this.setState({ tasks: tasks });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   };
   render() {
     return (
