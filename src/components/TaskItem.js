128d2962080da0261aa72f153739ec80f938aed9
uliana <uliana.neustroeva25@gmail.com>
2023-11-24 18:28:22 +0200

    Implement  with new functionality

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,18 +1,79 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class TaskItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: this.props.taskItem.task,
+      isEditing: false,
+    };
+  }
+
+  setEditingState = (isEditing) => {
+    this.setState({ isEditing: isEditing });
+  };
+  toggleTask = () => {
+    this.props.toggleTask(this.props.id);
+  };
+  deleteTask = () => {
+    this.props.deleteTask(this.props.id);
+  };
+  handleChange = (event) => {
+    this.setState({ task: event.target.value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.editTask(this.props.id, this.state.task);
+    this.setState({ isEditing: false });
+  };
   render() {
     return (
       <tr>
-        <td>
-            {this.props.taskItem.task}
-        </td>
-        <td>
-        <button>Edit</button>
-        <button>Delete</button>
-        </td>
+        {this.state.isEditing ? (
+          <>
+            <td>
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  value={this.state.task}
+                  onChange={this.handleChange}
+                  autoFocus
+                />
+              </form>
+            </td>
+            <td>
+              <button onClick={this.handleSubmit} type="submit">
+                Save
+              </button>
+              <button onClick={() => this.setEditingState(false)} type="button">
+                Back
+              </button>
+            </td>
+          </>
+        ) : (
+          <>
+            <td className="task" onClick={this.toggleTask}>
+              <input
+                type="checkbox"
+                readOnly
+                checked={this.props.taskItem.isCompleted}
+              />
+              <span
+                className={
+                  this.props.taskItem.isCompleted
+                    ? "completed"
+                    : "not-completed"
+                }
+              >
+                {this.props.taskItem.task}
+              </span>
+            </td>
+            <td>
+              <button onClick={() => this.setEditingState(true)}>Edit</button>
+              <button onClick={this.deleteTask}>Delete</button>
+            </td>
+          </>
+        )}
       </tr>
-    )
+    );
   }
 }
-
