27681bde154c31b37b522dab4f2267560cdaed2a
uliana <uliana.neustroeva25@gmail.com>
2023-11-24 18:28:57 +0200

    Implement task management functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,6 +18,20 @@ export default class Main extends React.Component {
     tasks.push({ task, isCompleted: false });
     this.setState({ tasks: tasks });
   };
+  toggleTask = (taskId) => {
+    const taskItem = tasks[taskId];
+    taskItem.isCompleted = !taskItem.isCompleted;
+    this.setState({ tasks: tasks });
+  };
+  deleteTask = (taskId) => {
+    tasks.splice(taskId, 1);
+    this.setState({ tasks: tasks });
+  };
+  editTask = (taskId, task) => {
+    const taskItem = tasks[taskId];
+    taskItem.task = task;
+    this.setState({ tasks: tasks });
+  };
   render() {
     return (
       <div>
@@ -25,7 +39,12 @@ export default class Main extends React.Component {
         <div>
           <CreateTask createTask={this.createTask} />
           <br />
-          <TaskList tasks={this.state.tasks}/>
+          <TaskList
+            tasks={this.state.tasks}
+            deleteTask={this.deleteTask}
+            editTask={this.editTask}
+            toggleTask={this.toggleTask}
+          />
         </div>
       </div>
     );
