5ea3de3c0f039f0e6d2419d7e0cc179d4571ba59
uliana <uliana.neustroeva25@gmail.com>
2023-11-24 17:04:01 +0200

    Implement TaskItem component

---
+++ src/components/TaskItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class TaskItem extends Component {
+  render() {
+    return (
+      <tr>
+        <td>
+            {this.props.taskItem.task}
+        </td>
+        <td>
+        <button>Edit</button>
+        <button>Delete</button>
+        </td>
+      </tr>
+    )
+  }
+}
+
